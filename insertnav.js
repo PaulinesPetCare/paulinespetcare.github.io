05dd904a720eb0ac5adf8c92507e9c9896105aee
PaulinesPetCare <99448566+PaulinesPetCare@users.noreply.github.com>
2022-08-12 22:58:11 +0100

    Init commit
    Initial commit of paulines pet care website

---
+++ insertnav.js
@@ -0,0 +1,8 @@
+navmenu = document.getElementById('nav-menu');
+navmenu.innerHTML = '<div class="img-container"><img src="img/nav-menu.png" alt="small logo of business"></div><ul class="nav-btn-list"><li><a href=index.html>Home</a></li><li><a href=aboutme.html>About Me</a></li><li><a href=pricelist.html>Price List</a></li><li><a href=contact.html>Contact</a></li></ul>'
+
+footer = document.getElementById('footer');
+footer.innerHTML = '<div class="footer-info"><div class="footer-text-container"><h3>MENU</h3><p><a href="index.html">Home</a></p><p><a href="aboutme.html">About Me</a></p><p><a href="pricelist.html">Price List</a></p><p><a href="contact.html">Contact</a></p></div><div class="footer-text-container"><h3>MY SERVICES</h3><p>Dog Walking</p><p>Dog Home Visits</p><p>Dog Home Boarding</p><p>Cat Sitting</p><p>Cat Visits</p><p>Small Animal Sitting / Home Visits (rabbits, hamsters, etc)</p></div><div class="footer-text-container"><h3>AREAS</h3><p>Bredbury</p><p>Woodley</p><p>Romiley</p><p>Marple</p></div><div class="footer-text-container"><h3>CALL ME OR EMAIL ME</h3><p>Call Paulineʼs Pet Care Services on 07596821800</p><p>Email paulineofwoodley@gmail.com</p></div></div><p class="copyright">Copyright - Paulines Dog Walking and Pet Care Services - 2022</p>'
+
+contactbar = document.getElementById('contact-bar');
+contactbar.innerHTML = '<p>Call: 07596821800</p><p>Email: paulineofwoodley@gmail.com</p>'
